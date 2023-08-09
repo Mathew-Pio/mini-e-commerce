571f5980283c6dbb383f867ac4c2f2b5c4911ff5
Okononfua Mathew <oseghaleokononfua@gmail.com>
2023-08-09 13:54:24 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const expressHbs = require('express-handlebars');
+
+const app = express();
+
+
+app.set('view engine', '.ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('error-page' , {path:"", pageTitle: 'Page not found'});
+});
+
+
+app.listen(3000);
---
+++ routes/shop.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const rootDir = require('../util/path')
+
+const router = express.Router();
+const adminData = require('./admin');
+
+router.get('/',(req, res, next) => {
+    const products = adminData.products;
+    res.render('shop', { prods: products, pageTitle: 'Shop' , path:'/products', hasProducts: products.length > 0, activeShop:true});
+});
+
+module.exports = router;
